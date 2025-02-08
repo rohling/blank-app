ab3e3df7b4d6bf208b5a471d867d36a4c8869358
Adair Rohling <rohling@users.noreply.github.com>
2025-02-08 16:20:36 +0000

    feat: uplods

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Define o diretório onde o PDF será salvo
+diretorio_pdf = 'uploads'
+
+# Verifica se o diretório existe, se não, cria um
+if not os.path.exists(diretorio_pdf):
+    os.makedirs(diretorio_pdf)
+
+# Cria o widget de upload de arquivo
+arquivo_pdf = st.file_uploader("Upload de arquivo PDF", type=["pdf"])
+
+# Se um arquivo foi carregado
+if arquivo_pdf is not None:
+    # Salva o arquivo no diretório especificado
+    caminho_arquivo = os.path.join(diretorio_pdf, arquivo_pdf.name)
+    
+    with open(caminho_arquivo, "wb") as pdf_file:
+        pdf_file.write(arquivo_pdf.getbuffer())
+    
+    st.success(f"Arquivo '{arquivo_pdf.name}' salvo com sucesso em '{caminho_arquivo}'.")
